940723d299958b8d7eadd4f58c020b674e4fea3c
Teachen <Teachen6@163.com>
2019-06-12 19:54:14 +0800

    更新渲染一级菜单

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ const router = express.Router();
 const navbar = require('../controllers/navbar'); 
 const subnavbar = require('../controllers/subnavbar'); 
 
-// var htm = require('./module/athlete_infohome.ejs');
+// var $$htm = require('./module/htm.ejs');
  // 一级模板
-// var tmplFn = require('./module/util.ejs');
+// const $$tmplFn = require('./module/util.ejs');
  // 二级模板：局部递归部分的模板，此时$$tmpl 和 $$recursiveTmpl 都是函数
 
 /* GET home page.查找导航栏 */
@@ -18,24 +18,25 @@ router.get('/', function(req, res, next) {
     resmsgs = navmsgs; 
   }).then(function(){ 
    
-    subnavbar.findAll().then(function(msgs){   
+    subnavbar.bulkCreate([
+      {organizeNo:1001,containNo:"",subnavName:"钮扣 合金扣"}, 
+      {organizeNo:1002,containNo:"",subnavName:"织带、线、绳"},
+      {organizeNo:1003,containNo:"",subnavName:"花边 机织花边"}, 
+      {organizeNo:1004,containNo:"",subnavName:"拉链 尼龙拉链"}, 
+      {organizeNo:1005,containNo:"",subnavName:"面料 印花面料"}, 
+      {organizeNo:1006,containNo:"",subnavName:"配饰 烫钻"}, 
+      {organizeNo:1007,containNo:"",subnavName:"衣架 木衣架"}, 
+      {organizeNo:1008,containNo:"",subnavName:"商标 织唛"}, 
+      {organizeNo:1009,containNo:"",subnavName:"填料 棉花"},  
+    ]).then(function(){
+      subnavbar.findAll().then(function(msgs){   
+   
+        res.render('index',{title: '辅料圈-辅料人的交流圈',navbars: resmsgs, subnavsort: msgs});
+  
+      });
+  
 
-      
-      // var subnavmsgs = JSON.parse(JSON.stringify(msgs));
-      // console.log("subnavmsgs数据"+subnavmsgs);
-       
-      // tojson(subnavmsgs[i].subnavContent,subnavmsgs[i].organizeNo);  
-      // 一级模板渲染使用的数据由数据和二级模板函数组成
-      // var datas = {
-      //   'list': subnavmsgs,
-      //   'tmplFn': $$recursiveTmpl 
-      // };  
-
-      // 将数据传入，一级模板 
-      // $("#subSort").append($$subnavsorttmpl(datas)); 
-      // var htm = tmplFn({list:j,'tmplFn': tmplFn, id: 'subSort'});
-      res.render('index',{title: '辅料圈-辅料人的交流圈',navbars: resmsgs, subnavsort: msgs}); 
-    });
+    })
 
   }); 
   
@@ -43,20 +44,40 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;   
 
+// function tojson(toj,no) {  
+//   for(var i in subnavmsgs){
+//     if(subnavmsgs[i].containNo == null&&no==""){  
+//       var obj = {
+//         name: subnavmsgs[i].subnavName,
+//         child: []
+//       };
+//       toj.push(obj);
+//       var j=obj['child'];
+//       tojson(j,subnavmsgs[i].organizeNo);
 
+//     }
+//     else if(subnavmsgs[i].containNo==no){
+//       var obj = {
+//         name: subnavmsgs[i].subnavName,
+//         child: []
+//       };
+//       toj.push(obj);
+//       var j=obj['child'];
+//       tojson(j,subnavmsgs[i].organizeNo);
+//     }
+//   }  
+// } 
+ 
+// tojson(subnavmsgs[i].subnavName,subnavmsgs[i].organizeNo); 
+ 
+      // var subnavmsgs = JSON.parse(JSON.stringify(msgs));
+      // console.log("subnavmsgs数据"+subnavmsgs);
+      // 一级模板渲染使用的数据由数据和二级模板函数组成
+      // var datas = {
+      //   'list': subnavmsgs,
+      //   'tmplFn': $$tmplFn
+      // };  
 
-      // function tojson(toj,no) { 
-
-      //   for(var i in subnavmsgs){
-      //     if(subnavmsgs[i].containNo == no){  
-      //       var obj = {
-      //         name: subnavmsgs[i].subnavName,
-      //         child: []
-      //       };
-      //       toj.push(obj);
-      //       var j=obj['child'];
-      //       tojson(j,subnavmsgs[i].organizeNo);
-
-      //     }
-      //   }  
-      // } 
+      // 将数据传入，一级模板 
+      // $("#subSort").append($$subnavsorttmpl(datas)); 
+      // var datas = tmplFn({list:subnavmsgs,'tmplFn': $$htm}); 
